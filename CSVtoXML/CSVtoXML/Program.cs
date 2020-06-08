e36aa899f8099988554928eaafc313a1e88725a8
Bravo Yeung <1530676929@qq.com>
2020-06-08 08:35:12 +0800

    Init this project (.NET core).

---
+++ CSVtoXML/CSVtoXML/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CSVtoXML
+{
+    class Program
+    {
+        private static void AddContentForEachLine(string line, ref XElement xmlTree)
+        {
+            var currentTree = new XElement("Item");
+            string[] slices = line.Split(",");
+
+            for (int i = 0; i < slices.Count(); i++)
+                currentTree.Add(new XElement($"Column{i}", slices[i].ToString()));
+
+            xmlTree.Add(currentTree);
+        }
+
+        static void Main(string[] args)
+        {
+            var basePath = Environment.CurrentDirectory;
+            var lines = File.ReadAllLines(Path.Combine(basePath, "../../..", @"input.csv"));
+
+            var xmlTree = new XElement("Root");
+
+            foreach (var line in lines)
+            {
+                AddContentForEachLine(line, ref xmlTree);
+            }
+
+            xmlTree.Save(Path.Combine(basePath, "../../..", @"output.xml"));
+        }
+    }
+}
